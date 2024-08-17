d5a5ecf6eca178f8c44625eacf2fc7da150f15ae
Rogena5 <rogenaramze10@gmail.com>
2024-08-17 11:36:25 +0300

    fixed darkMode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Container from '@/components/global/Container'
+import Providers from "./Providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,12 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <Providers>
         <Navbar/>
        <Container className="py-20">
        {children}
        </Container>
+        </Providers>
       </body>
     </html>
   );
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,18 @@
+'use client'
 import React from 'react'
 import { ThemeProvider } from 'next-themes';
 
-function providers({children}: {children: React.ReactNode}) {
+function Providers({ children }: { children: React.ReactNode }) {
   return (
-   <>
-   <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>{children}</ThemeProvider>
-   </>
+    <>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='system' enableSystem
+        disableTransitionOnChange
+      >{children}
+      </ThemeProvider>
+    </>
   )
 }
 
-export default providers
+export default Providers
