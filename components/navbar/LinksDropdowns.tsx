16115c9b4931da17baecdbcb7088d7a9b68e93d5
Rogena5 <rogenaramze10@gmail.com>
2024-08-17 12:44:12 +0300

    LinksDropdown Component - Initial Setup

--- components/navbar/LinksDropdowns.tsx
+++ components/navbar/LinksDropdowns.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {Button} from '@/components/ui/button'
+import Link from 'next/link'
+import {links} from '../../utils/links'
+import { LuAlignLeft } from 'react-icons/lu'
 export default function LinksDropdowns() {
   return (
-    <div>LinksDropdowns</div>
+ <>
+ <DropdownMenu>
+  <DropdownMenuTrigger asChild>
+    <Button variant='outline' className='flex gap-4 max-w-[100px]'>
+      <LuAlignLeft className='h-6 w-6'/>
+    </Button>
+  </DropdownMenuTrigger>
+  <DropdownMenuContent align='start' className='w-48' sideOffset={10}>
+    {links.map((link)=> {return <DropdownMenuItem key={link.href}>
+  <Link href={link.href} className='capitalize w-full'>{link.label}</Link>
+    </DropdownMenuItem>})}
+  </DropdownMenuContent>
+ </DropdownMenu>
+ </>
   )
 }
